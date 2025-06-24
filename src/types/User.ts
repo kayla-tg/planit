58d96ee89a51d9ee06807920272c71504a7ee85b
Kayla George <kaylatrinitygeorge@gmail.com>
2025-06-24 17:08:20 -0400

    getUserByEmail method added to User

--- src/types/User.ts
+++ src/types/User.ts
@@ -4,3 +4,22 @@ export interface User {
     email: string, 
     password: string
 }
+
+const getUsers = async () => {
+    const url = `http://localhost:8000/users`
+    const response = await fetch(url)
+    if (!response.ok) {
+        throw new Error("Network request failed")
+    }
+    return await response.json() as User[]  
+}
+
+export const getUserByEmail = async (email: string): Promise<User | null> => {
+    const users = await getUsers()
+    users.forEach((user: User) => {
+        if ((user.email === email)) {
+            return user
+        }
+    })
+    return null
+}
